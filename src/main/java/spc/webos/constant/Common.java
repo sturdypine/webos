f4301d7ca555ceec2d7aaa65f2978c6219dca53c
陈劲松 <sturdypine@icloud.com>
2017-03-17 13:56:00 +0800

    commit

--- src/main/java/spc/webos/constant/Common.java
+++ src/main/java/spc/webos/constant/Common.java
@@ -3,7 +3,7 @@ package spc.webos.constant;
 public class Common
 {
 	// 当前版本信息
-	public static final String VERSION = "9.3.9b";
+	public static final String VERSION = "9.3.9";
 	public static final String VERSION_DATE = "20170317";
 
 	// jms. properties
