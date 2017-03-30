43ed6b8666ef44450748b3b6599f96481dfb1ad4
陈劲松 <sturdypine@icloud.com>
2017-03-30 11:22:57 +0800

    commit

--- bin/start.sh
+++ bin/start.sh
@@ -10,6 +10,7 @@ fi
 
 cd $(dirname $0)/../apps
 webosPath=`pwd`
+# check exists "apps/xx/conf/jvm.properties"
 if [ -f "${module}/conf/${jvm}.properties" ]
 then
 	cd ${module}
@@ -23,21 +24,21 @@ fi
 bizjar=""
 webos=""
 CLASSPATH=
-# sacn apps/xx/lib/mule/*.jar
+# scan apps/xx/lib/mule/*.jar
 if [ -d "lib/mule/" ]
 then
 	for jar in lib/mule/*.jar; do
 	    CLASSPATH=$jar:$CLASSPATH
 	done
 fi
-# sacn apps/xx/lib/ext/*.jar
+# scan apps/xx/lib/ext/*.jar
 if [ -d "lib/ext/" ]
 then
 	for jar in lib/ext/*.jar; do
 	    CLASSPATH=$jar:$CLASSPATH
 	done
 fi
-# sacn apps/xx/lib/*.jar
+# scan apps/xx/lib/*.jar
 for jar in lib/*.jar; do
     CLASSPATH=$jar:$CLASSPATH
     bizjar=${jar:4}:$bizjar
